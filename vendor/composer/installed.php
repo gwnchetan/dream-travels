357765ffb49bbcfdabe489f83d8fd799bf8995cb
chetan <csakre634@gmail.com>
2024-11-07 10:58:38 +0530

    google api fix

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '77153c478345e7de4ddbbf0cf20436b46074668e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '77153c478345e7de4ddbbf0cf20436b46074668e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -38,9 +38,9 @@
             'dev_requirement' => false,
         ),
         'google/apiclient-services' => array(
-            'pretty_version' => 'v0.374.0',
-            'version' => '0.374.0.0',
-            'reference' => '5f6060df419f4f72dcc970197f9a9b1613cecc62',
+            'pretty_version' => 'v0.380.0',
+            'version' => '0.380.0.0',
+            'reference' => 'a706b940e309d6cd858eee6a394e89fa17310c93',
             'type' => 'library',
             'install_path' => __DIR__ . '/../google/apiclient-services',
             'aliases' => array(),
